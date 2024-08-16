46d063c69e82666e24829de941b00c7be047e83f
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-08-16 17:52:58 +0900

    cell の表示

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Hashimoto", "Hachioji", "Shibuya"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //MARK: - Tabelview Datasouce Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
 }
 
