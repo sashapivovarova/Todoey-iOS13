079fc8b37c16d1f7fe536fb9d3526887975b24a7
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-08-21 20:47:49 +0900

    addButtonPressed の追加

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Hashimoto", "Hachioji", "Shibuya"]
+    var itemArray = ["Hashimoto", "Hachioji", "Shibuya"]
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     //MARK: - Tableview Datasouce Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -41,6 +40,29 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will happen once the user clicks the Add Item Button on our UIAlert
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField{ (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
