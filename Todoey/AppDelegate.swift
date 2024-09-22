87e2954c230e5405bd76bc35599c56c8762dc702
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-09-22 20:48:26 +0900

    Realm fileURL の出力

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -18,12 +18,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "Sasha"
+        data.age = 20
+        
         do {
-            let lealm = try Realm()
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
         } catch {
             print("Error initialising new realm, \(error)")
         }
-        
         return true
     }
     func applicationWillTerminate(_ application: UIApplication) {
