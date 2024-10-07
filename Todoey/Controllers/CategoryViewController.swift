051c7c90f980fc3d0db0f89a9d93f6a0c2f31f06
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-10-07 20:58:47 +0900

    expansionStyle の追加

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -119,4 +119,10 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
         
         return [deleteAction]
     }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
 }
